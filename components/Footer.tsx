416ad40654511644bc83b35d19edaabab6a11204
uniters_ngo <uniters-fundation@tutanota.com>
2022-05-25 23:18:36 +0200

    update: add basic footer

---
+++ components/Footer.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { VercelBanner } from "./VercelBanner";
+
+export const Footer = () => (
+  <footer style={{ color: "gray", position: "fixed", bottom: 0 }}>
+    <VercelBanner />
+  </footer>
+);
